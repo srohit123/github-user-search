4e686510180f89a6b3b23f559c09f907d4ada31b
Rohit <srohit0608@gmail.com>
2023-06-21 23:28:36 +0530

    Added new error messages.

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,4 +1,6 @@
 export const ERRORS = {
   USE_SNACKBAR_CONTEXT: 'useSnackBar must be used within an SnackBarProvider',
-}
-
+  FIELD_REQUIRED: 'This field is required!',
+  NO_RESULT_FOUND: 'No result found',
+  SERVER: 'Something Went wrong. Please Try Again.'
+};
